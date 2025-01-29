c1e6cc8d9d66a258f342b5890cb66ec1f9ea4ec6
patitapaban <patitapabanpanda046@gmail.com>
2025-01-29 16:46:52 +0530

    reversed string without inbuilt function

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 // Write a function to check if a number is even or odd.
 
-const evenOdd = (num) => {
-  if (num % 2 == 0) {
-    return "even";
-  } else {
-    return "odd";
-  }
-};
+// const evenOdd = (num) => {
+//   if (num % 2 == 0) {
+//     return "even";
+//   } else {
+//     return "odd";
+//   }
+// };
 
-console.log(evenOdd(3));
-console.log(evenOdd(4));
-console.log(evenOdd(30));
+// console.log(evenOdd(3));
+// console.log(evenOdd(4));
+
+// Implement a function to reverse a string.
+
+// const str = "hellobapunu";
+// const reversed = str.split("").reverse().join("");
+
+// console.log(reversed);
+
+const reverseStr = (str) => {
+  let reversed = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+};
+console.log(reverseStr("aidni"));
