b4403933a8e95b205171118d110667bedad372ce
patitapaban <patitapabanpanda046@gmail.com>
2025-04-14 17:22:16 +0530

    remove duplicate from an array

--- index.js
+++ index.js
@@ -1,100 +1,39 @@
 // Write a function to check if a number is even or odd.
 
 const evenOdd = (num) => {
-  if (num % 2 == 0) {
-    return "even";
+  if (num % 2 === 0) {
+    return "Even";
   } else {
-    return "odd";
+    return "Odd";
   }
 };
 
 // console.log(evenOdd(3));
-// console.log(evenOdd(4));
+// console.log(evenOdd(32));
 
 // Implement a function to reverse a string.
 
-const str = "hellobapunu";
-const reversed = str.split("").reverse().join("");
+const str = "I Love Cricket";
 
+// In built Method
+
+let reversed = str.split("").reverse().join("");
 // console.log(reversed);
 
-const reverseStr = (str) => {
-  let reversed = "";
+// using loop // also check palindrome
 
-  for (let i = str.length - 1; i >= 0; i--) {
-    reversed += str[i];
-  }
-  return reversed;
-};
-// console.log(reverseStr("aidni"));
-
-// Write a program to find the largest number in an array.
-
-const number = [1, 23, 47, 98, 76];
-
-function largestElement(arr) {
-  let max = arr[0];
-
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
-    }
-  }
-  return max;
-}
-// console.log(largestElement(number));
-
-// Create a function to count the number of vowels in a string.
-
-function checkVowels(str) {
-  let count = 0;
-  const vowels = "aeiouAEIOU";
-
-  for (let i = 0; i < str.length; i++) {
-    if (vowels.includes(str[i])) {
-      count++;
-    }
-  }
-  return count;
-}
-// console.log(checkVowels("HELLO WORLFe"));
-
-// Write a function to check if a word is a palindrome.
-
-const isPalindrome = (str) => {
+const reverseString = (str) => {
   let reverse = "";
 
   for (let i = str.length - 1; i >= 0; i--) {
     reverse += str[i];
   }
-
-  return reverse === str ? "palindrome" : "not palindrome";
-};
-// console.log(isPalindrome("oho"));
-// console.log(isPalindrome("Hello"));
-
-// check number is palindrome or not
-
-const isNumberPalindrome = (num) => {
-  if (num < 0) return "Not Palindrome";
-  let original = num.toString();
-
-  let reverse = "";
-
-  for (let i = original.length - 1; i >= 0; i--) {
-    reverse += original[i];
+  if (reverse === str) {
+    return "palindrome";
+  } else {
+    return "not Palindrome";
   }
-
-  return original === reverse ? "Palindrome" : "not Palindrome";
 };
-// console.log(isNumberPalindrome(-121));
-// console.log(isNumberPalindrome(12));
-// console.log(isNumberPalindrome(11));
 
-// Implement a program to remove duplicates from an array.
-const arr = [1, 2, 298, 9, 8, 9, 7, 8, 9, 8, 3, 2, 2, 34, 34];
-
-const removeDuplicate = (arr) => {
-  return [...new Set(arr)];
-};
-console.log(removeDuplicate(arr));
+// console.log(reverseString("oho"));
+// console.log(reverseString("hho"));
